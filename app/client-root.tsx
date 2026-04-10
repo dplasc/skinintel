58d68f215a8cfcb5efda436c3a31cb27e7af30b3
Oglas <tvoj@email.com>
2026-04-10 18:07:57 +0200

    Remove template theme customizer from dashboard

--- app/client-root.tsx
+++ app/client-root.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { AppSidebar } from "@/components/app-sidebar";
-import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
-import ThemeCustomizer from "@/components/theme-customizer/theme-customizer";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { ReactNode } from "react";
@@ -32,9 +30,7 @@ export function ClientRoot({
           <div className="dashboard-body bg-neutral-100 dark:bg-[#1e2734] md:p-6 p-4 flex-1">
             {children}
           </div>
-          <Footer />
         </main>
-        <ThemeCustomizer />
         <Toaster position="top-center" reverseOrder={false} />
       </SidebarProvider>
     </ThemeProvider>
